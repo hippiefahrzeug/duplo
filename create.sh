c54eaed1a2f85783b892f25bc341da706a6232d0
alvi <git@o08.net>
2011-09-16 19:00:10 +0200

    download groovy jar if it doesn't exist

--- create.sh
+++ create.sh
@@ -3,7 +3,19 @@
 rm -rf classes
 mkdir -p classes
 mkdir -p dist
-cp /usr/share/java/groovy-all-1.7.4.jar dist/duplo.jar
+mkdir -p archive
+
+if [ ! -e "archive/groovy-all-1.7.4.jar" ]
+then
+if [ -e "/usr/share/java/groovy-all-1.7.4.jar" ] 
+then
+cp /usr/share/java/groovy-all-1.7.4.jar archive/groovy-all-1.7.4.jar
+else
+wget -U duplo http://repo1.maven.org/maven2/org/codehaus/groovy/groovy-all/1.7.4/groovy-all-1.7.4.jar -O archive/groovy-all-1.7.4.jar
+fi
+fi
+
+cp archive/groovy-all-1.7.4.jar dist/duplo.jar
 cd classes
 groovyc ../src/duplo.groovy
 jar uvf ../dist/duplo.jar .
